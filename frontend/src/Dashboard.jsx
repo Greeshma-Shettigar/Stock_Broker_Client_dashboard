a433b7af381d1c0e93f6120f95b9ca9e02684763
Greeshma Shettigar <shettigargreeshma@gmail.com>
2026-02-10 16:19:55 +0530

    Remove unused import of useRef in Dashboard component

--- frontend/src/Dashboard.jsx
+++ frontend/src/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState} from "react";
 import { io } from "socket.io-client";
 import { useNavigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
